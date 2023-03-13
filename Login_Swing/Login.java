d2953f3d647544cd46b3f07c96dc0eaa9c53a23d
Nischal Shrestha <nischalstha9@gmail.com>
2023-03-13 19:20:20 +0545

    awt

--- Login_Swing/Login.java
+++ Login_Swing/Login.java
@@ -1,15 +1,20 @@
 package Login_Swing;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Login {
+public class Login implements ActionListener {
+    JTextField tfUser;
+    JPasswordField tfPassword;
+    JButton JSubmit;
+
     public Login() {
         JFrame f = new JFrame("Login");
         JLabel lUser = new JLabel("Username");
         lUser.setBounds(20, 20, 150, 25);
         f.add(lUser);
 
-        JTextField tfUser = new JTextField();
+        tfUser = new JTextField();
         tfUser.setBounds(180, 20, 150, 25);
         f.add(tfUser);
 
@@ -18,18 +23,29 @@ public class Login {
         lPassword.setBounds(20, 55, 150, 25);
         f.add(lPassword);
 
-        JTextField tfPassword = new JTextField();
+        tfPassword = new JPasswordField();
         tfPassword.setBounds(180, 55, 150, 25);
         f.add(tfPassword);
 
-        JButton JSubmit = new JButton();
-        JSubmit.setText("Login ");
+        JSubmit = new JButton();
+        JSubmit.setText("Login");
         JSubmit.setBounds(180, 90, 150, 25);
+        JSubmit.addActionListener(this);
         f.add(JSubmit);
 
         f.setSize(500, 500);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String username = tfUser.getText();
+        String password = tfPassword.getText();
+        if (username.equals("admin") && password.equals("admin")) {
+            System.out.println("Authenticated!");
+        } else {
+            System.out.println("Wrong Credentials!");
+        }
     }
 
     public static void main(String[] args) {
